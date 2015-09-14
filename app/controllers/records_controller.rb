2ddd95a044bb98974ac670074b57a08f07046b02
ikiyoyuki <kiyoyuki.ishikawa@gmail.com>
2015-09-14 20:25:06 +0900

    投稿機能実装

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -2,4 +2,16 @@ class RecordsController < ApplicationController
   def index
     @records = Record.all
   end
+
+  def new
+  end
+
+  def create
+    Record.create(record_params)
+  end
+
+  private
+  def record_params
+    params.permit(:name, :image, :text)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  get 'records' => 'records#index'
+  get 'records' => 'records#index' # レコード一覧画面
+  get 'records/new' => 'records#new' # レコード投稿画面
+  post 'records' => 'records#create' # レコード投稿機能
 end
